c3bda69e94e7b75dbef8dec13451a8d312e1a879
Tonold <ancientsflame@mail.ru>
2025-09-28 13:52:56 +0700

    after adding fixrures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from goods.models import Categories, Products
 
-admin.site.register(Categories)
-admin.site.register(Products)
+# админка переделанна на более расширенную.
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+@admin.register(Categories)
+class CategiesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
