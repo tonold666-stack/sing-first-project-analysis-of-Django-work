b21760931511561f6cfecfb60507340a2dcdc241
Tonold <ancientsflame@mail.ru>
2025-10-08 16:17:12 +0700

    The product filter on the site page and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from atexit import register
+from urllib.parse import urlencode
 from django import template
 
 from goods.models import Categories
@@ -9,3 +10,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
